becf97f94c20fcb6232e9fbeced97029f963d671
franciscoldb <fluis905@gmail.com>
2022-11-18 00:25:50 -0300

    ajuste ex 09 e nome de arqs

--- Aula 09/exercicio09.js
+++ Aula 09/exercicio09.js
@@ -20,9 +20,16 @@ console.log(Circulo1.calc_area());
 
 //3. Altere seu objeto círculo para utilizar a sintaxe com uma função construtora chamada Circulo
 
-Circulo1.Circulo = (raio) => { this.raio = raio; }
-Circulo1.Circulo(4);
-console.log(Circulo1.raio);
+function criarCirculo(raio){
+    return {
+        raio,
+        calc_area,
+        calc_perimetro
+    }
+}
+const circulo3 = criarCirculo(4);
+
+console.log(circulo3.raio);
 //4. Crie 2 círculos de raios diferentes. E imprima sua área e perímetro
 
 const c1 = {
@@ -40,69 +47,56 @@ console.log("c1: area = " + c1.area() + "    perimetro = " + c1.perimetro());
 console.log("c2: area = " + c2.area() + "    perimetro = " + c2.perimetro());
 
 //5. Refatore o seu código para o círculo utilizar a sintaxe de classes
+//6. Crie o atributo do raio. Torne-o privado.
+//7. Adicione em sua classe um setter em que, caso o raio seja negativo, retire o sinal (um raio setado como -3 se tornaria 3)
+//8. Adicione getters para o método raio e também para a área e o perímetro
+//9. Sobreponha o método toString() da sua classe Circulo para imprimi-la como Circulo de raio ${raio}
+//10. Crie um método estático criar que aceita uma lista como parâmetro e gera um círculo para cada número positivo em seu interior, ignore os parâmetros que não puderem ser convertidos para números.
 
 class Circulo{
-    raio;
+    #raio;
     constructor(raio = 0){
-        this.raio = raio;
+        this.#raio = raio;
     }
-    get_area = calc_area();
-    get_perimetro = calc_perimetro();
+    get area(){
+        return Math.PI * this.#raio * this.#raio;
+    }
+    set area(area){ 
+        this.#raio = Math.sqrt(area/Math.PI);
+    }
+    get perimetro(){
+        return 2 * Math.PI * this.#raio;
+    }
+    set perimetro(perimetro) {
+        this.#raio = perimetro/(2*Math.PI);
+    }
+    get raio() {
+        return this.#raio
+    }
+    set raio(raio) {
+        this.#raio = Math.abs(raio);
+    }
+    toString(){
+        return `Circulo de raio ${this.#raio}`;
+    }
+    static criar(...lista){
+        return lista
+            .filter((raio) => typeof raio == 'number' && raio > 0)
+            .map(raio => new Circulo(raio));
+    }
 }
-
 const circ5 = new Circulo(5);
 
-//6. Crie o atributo do raio. Torne-o privado.
-class CirculoPrivado{
-    #raio;
-    constructor(raio = 0){
-        this.#raio = raio;
-    }
-}
-const cp = new CirculoPrivado(3);
+console.log(`\nraio = ${circ5.raio}`);
+console.log(circ5.area);
+console.log(circ5.perimetro);
+console.log(circ5.toString() + "\n");
 
-//7. Adicione em sua classe um setter em que, caso o raio seja negativo, retire o sinal (um raio setado como -3 se tornaria 3)
+const circulos = Circulo.criar(1,2,"eee",3,4,5);
 
-Circulo.prototype.set_raio = (raio) => {
-    if(raio < 0){
-        raio = raio*(-1);
-    }
-    this.raio = raio;
-    console.log('raio == ' + raio + '  this.raio == ' + this.raio);
-}
-const circ11 = new Circulo();
-circ11.set_raio(-1);
-console.log('circ11 == ' + circ11.raio);
+circulos.forEach(c => console.log(c.toString()));
 
 
-//8. Adicione getters para o método raio e também para a área e o perímetro
-
-/*  CirculoPrivado.prototype.get_raio = () => { return this.#raio; }
-    CirculoPrivado.prototype.get_area = () => { return (PI*this.#raio*this.#raio); }
-    CirculoPrivado.prototype.get_perimetro = () => { return (2*PI*this.#raio); }    */
-
-class CirculoPrivado8{
-    #raio;
-    constructor(raio = 0){
-        this.#raio = raio;
-    }
-    get_raio(){ return this.#raio; }    
-    get_area(){ return (PI*this.#raio*this.#raio); }
-    get_perimetro(){ return (2*PI*this.#raio); }
-}
-const circ8 = new CirculoPrivado8(8);
-console.log('circ8 raio = ' + circ8.get_raio() + '  area = ' + circ8.get_area() + '  perimetro = ' + circ8.get_perimetro());
-
-//9. Sobreponha o método toString() da sua classe Circulo para imprimi-la como Circulo de raio ${raio}
-
-Circulo.toString = () =>{
-    console.log(`Circulo de raio ${this.raio}`);
-}
-const circ9 = new Circulo(9);
-
-console.log(circ9);
-
-//10. Crie um método estático criar que aceita uma lista como parâmetro e gera um círculo para cada número positivo em seu interior, ignore os parâmetros que não puderem ser convertidos para números.
 
 
 
