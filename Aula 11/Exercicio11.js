059dfb553be30ce9dac9abc95d26a314049ef6e3
franciscoldb <fluis905@gmail.com>
2022-11-18 20:43:22 -0300

    add parcial ex11

--- Aula 11/Exercicio11.js
+++ Aula 11/Exercicio11.js
@@ -37,6 +37,36 @@ passo.
 console.log("---------------------");
 console.log("EXERCICIO 02\n");
 
+function calc_fibo(n) {
+    let resposta = [];
+    let num = 0;
+    let num1 = 1;
+    let num2 = 0;
+    for(let i = 0; i < n; i++){
+        num2 = num1;
+        num1 = num;
+        num = num1 + num2;
+        resposta.push(num);
+    }
+    return resposta;
+}
+const fibo = calc_fibo(6); 
+
+const delayedPrint = (text, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(text);
+            resolve();
+        }, delay)
+    })
+}
+async function printFibo(){
+    //console.log(fibo.length());
+    for(let i = 0; i < fibo.length() - 1; i++){
+        await delayedPrint(`${fibo[i]} `,500);
+    }
+}
+printFibo();
 
 /*Exercicio 03
 Usando programação assíncrona, crie uma webpage que
